05e6c40336a4e25cd385bfb4524ecc6a8cb2ce00
FRCTEAM1474 <frcteam1474@gmail.com>
2026-03-02 20:09:28 -0500

    3.2.26_DW_CM_Finished All Autos.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -85,8 +85,10 @@ public class RobotContainer {
             Commands.runOnce(() -> shooter.stopShooter())
         ));
 
-        NamedCommands.registerCommand("Shoot10Balls", shootMultipleBalls(10));
-        NamedCommands.registerCommand("Shoot20Balls", shootMultipleBalls(20));
+    // --- NEW: Register the Multi-Shot Commands for Auto! ---
+    NamedCommands.registerCommand("Shoot10Balls", shootMultipleBalls(10));
+    NamedCommands.registerCommand("Shoot20Balls", shootMultipleBalls(20));
+     NamedCommands.registerCommand("Shoot30Balls", shootMultipleBalls(30));
 
         // 3. Build the Auto Chooser
         autoChooser = AutoBuilder.buildAutoChooser();
