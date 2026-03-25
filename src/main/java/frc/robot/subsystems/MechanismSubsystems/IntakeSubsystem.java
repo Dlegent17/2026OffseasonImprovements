65894b42c822504f02114ca7d041aaade9167e60
Dlegent17 <dylwarren17@gmail.com>
2026-03-25 08:38:01 -0400

    Remove duplicate import of IdleMode
    Removed duplicate import of IdleMode from SparkBaseConfig.

--- src/main/java/frc/robot/subsystems/MechanismSubsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/MechanismSubsystems/IntakeSubsystem.java
@@ -6,7 +6,7 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
